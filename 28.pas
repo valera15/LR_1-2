871b8c8d9144f20ae26956f20ecbfb6a528b9a97
Leraron159 <144227244+valera15@users.noreply.github.com>
2023-09-16 21:02:54 +0300

    Add files via upload

---
+++ 28.pas
@@ -0,0 +1,10 @@
+﻿program zad_28;
+var n, x: integer;
+  begin
+    writeln ('На лугу пасется');
+    read (n);
+    n:=n mod 10;
+    if n=1 then write ('корова');
+    if (n>1) and (n<5) then write ('коровы');
+    if ((n>=5) and (n<=9)) or (n=0) then writeln ('коров'); 
+  end.
