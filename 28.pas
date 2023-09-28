65b6d443ea3b22381bd94eae2e9e827a811ac9a2
Leraron159 <144227244+valera15@users.noreply.github.com>
2023-09-28 15:13:25 +0300

    Update 28.pas

--- 28.pas
+++ 28.pas
@@ -1,10 +1,14 @@
-﻿program zad_28;
-var n, x: integer;
+program zad_28;
+var n: integer;
   begin
     writeln ('На лугу пасется');
     read (n);
-    n:=n mod 10;
-    if n=1 then write ('корова');
-    if (n>1) and (n<5) then write ('коровы');
-    if ((n>=5) and (n<=9)) or (n=0) then writeln ('коров'); 
+    if (n>=11) and (n<=20) then
+    write ('korov') else
+     begin
+       n:=n mod 10;
+       if n=1 then write ('korova');
+       if (n>1) and (n<5) then write ('korovy');
+       if ((n>=5) and (n<=9)) or (n=0) then write ('korov'); 
+     end;
   end.
