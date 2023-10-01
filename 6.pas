f69227b53c1112046616cd3fa3600d58c95995ce
Leraron159 <144227244+valera15@users.noreply.github.com>
2023-10-01 21:19:55 +0300

    Update 6.pas

--- 6.pas
+++ 6.pas
@@ -4,6 +4,5 @@ begin
   writeln ('Введите трехзначное число');
   readln(x);
   a:= (x mod 10 *100) + ((x mod 100)div 10*10) + x div 100;
-  writeln ('Перевернутое число - ', a);
-  
+  writeln ('Перевернутое число - ', a); 
 end.
